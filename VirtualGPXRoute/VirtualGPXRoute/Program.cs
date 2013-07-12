0a0f537c0a7dda4f0b7e48ed01a5310c4c4c8872
Eitan Behar <eitan@baconao.net>
2013-07-12 14:25:24 +0300

    Adding Solution Virtual Route

---
+++ VirtualGPXRoute/VirtualGPXRoute/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynastream.Fit; // downloaded from http://www.thisisant.com/resources/fit // also decoder code used from corresponding example
+using System.IO;
+using System.Xml.Linq;
+
+namespace VirtualGPXRoute
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string fitFile = @"c:\temp\2013-07-10-08-00-25.fit";
+            string tcxFile = @"c:\temp\2013-07-10-08-00-25.tcx";
+
+            Tcx tcx = Fit.ReadFitFileIntoTcxObject(fitFile);
+
+
+           //actual distance, read from static device, elliptical track
+            tcx.DistanceMeters = 4000;
+            tcx.Calories = 389;
+            tcx.Sport = "Elliptical training";
+
+            int numberOfTrackPoints = tcx.TrackpointList.Count();
+            double distPerPoint = (double) (tcx.DistanceMeters / 1000) / numberOfTrackPoints;
+
+            tcx.TotalTimeSeconds = (tcx.TrackpointList[numberOfTrackPoints - 1].Time - tcx.TrackpointList[0].Time).TotalSeconds;
+
+            tcx.TrackpointList[0].LatitudeDegrees = 31.245080;
+            tcx.TrackpointList[0].LongitudeDegrees = 34.812100;
+
+            for (int i = 1; i < tcx.TrackpointList.Count(); i++)
+            {
+                tcx.TrackpointList[i].LongitudeDegrees = tcx.TrackpointList[i - 1].LongitudeDegrees;
+                tcx.TrackpointList[i].LatitudeDegrees = tcx.TrackpointList[i -1].LatitudeDegrees;
+                Utils.SetPoint(tcx.TrackpointList[i - 1], tcx.TrackpointList[i], distPerPoint);
+            }
+            
+            tcx.Save(tcxFile);
+
+           // Console.ReadKey();
+            return;
+
+        }
+
+    }
+
+}
---
+++ VirtualGPXRoute/VirtualGPXRoute/Tcx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace VirtualGPXRoute
+{
+    class Tcx
+    {
+        public string Sport = String.Empty;
+        public string Id = String.Empty;
+        public DateTime StartTime ;
+        public double DistanceMeters;
+        public double TotalTimeSeconds;
+        public int Calories;
+        
+        public List<Trackpoint> TrackpointList = new List<Trackpoint>();
+
+        public void Save(String Filename)
+        {
+            XElement xTrack = new XElement("Track");
+
+            foreach (Trackpoint trackPoint in TrackpointList)
+            {
+                xTrack.Add(trackPoint.GetTrackpoint());
+            }
+
+            XElement xTcx = new XElement("Activities",
+                new XElement("Activity", new XAttribute("Sport", Sport),
+                    new XElement("Id", Id),                   
+                    new XElement("Lap", new XAttribute("StartTime", Trackpoint.ConvertDate(StartTime)),
+                         new XElement("DistanceMeters", DistanceMeters),
+                         new XElement("Calories", Calories),
+                         new XElement("TotalTimeSeconds", TotalTimeSeconds),
+                        xTrack)));
+
+            xTcx.Save(Filename);
+
+        }
+
+    }
+}
