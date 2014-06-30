e204e68d35b27cc7cb469ec948afeafc3b76c138
Eitan Behar <eitanb@dbmotion.com>
2014-06-30 19:44:29 +0300

    FTP Upload

--- Build/BuildFitExport.ps1
+++ Build/BuildFitExport.ps1
@@ -6,37 +6,43 @@ $BaseFolder = 'C:\Personal\gpx'
 
 $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.EXE"
 
-Write-Host "Cleaning the build directory..."
-Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit.sln /p:Configuration=Release /p:Platform='Any CPU' /t:clean /v:quiet /nologo"
+#Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit.sln /p:Configuration=Release /p:Platform='Any CPU' /t:clean /v:quiet /nologo"
+#Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit.sln /p:Configuration=Release /p:Platform='Any CPU' /t:rebuild /v:quiet /nologo"
 
-Write-Host "Building Executable application..."
-Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit.sln /p:Configuration=Release /p:Platform='Any CPU' /t:rebuild /v:quiet /nologo"
+$newExeVersion = '1.0.0.0'
+$newExeVersionFolder = '1_0_0_0'
 
-$newExeVersion = Get-ChildItem $BaseFolder\ExportFit\ExportFit\bin\Release\ExportFit.exe | Select-Object -ExpandProperty VersionInfo | % { $_.FileVersion }
-$newLibVersion = Get-ChildItem $BaseFolder\ExportFit\ExportFit\bin\Release\FitUtils.dll | Select-Object -ExpandProperty VersionInfo | % { $_.FileVersion }
+#$ProjectXml = [xml](Get-Content $BaseFolder\ExportFit\ExportFit\ExportFit.csproj)
+#$ns = new-object Xml.XmlNamespaceManager $ProjectXml.NameTable
+#$ns.AddNamespace('msb', 'http://schemas.microsoft.com/developer/msbuild/2003')
+#$AppVersion = $ProjectXml.SelectSingleNode("//msb:Project/msb:PropertyGroup/msb:ApplicationVersion", $ns)
+#$AppVersion.InnerText = $newExeVersion
+#$TargetPath = Resolve-Path "$BaseFolder\ExportFit\ExportFit\ExportFit.csproj"
+#$ProjectXml.Save($TargetPath)
 
-Write-Host "Building ClickOnce installer..."
-#
-# Because the ClickOnce target doesn't automatically update or sync the application version
-# with the assembly version of the EXE, we need to grab the version off of the built assembly
-# and update the Executable.csproj file with the new application version.
-#
-$ProjectXml = [xml](Get-Content $BaseFolder\ExportFit\ExportFit\ExportFit.csproj)
-$ns = new-object Xml.XmlNamespaceManager $ProjectXml.NameTable
-$ns.AddNamespace('msb', 'http://schemas.microsoft.com/developer/msbuild/2003')
-$AppVersion = $ProjectXml.SelectSingleNode("//msb:Project/msb:PropertyGroup/msb:ApplicationVersion", $ns)
-$AppVersion.InnerText = $newExeVersion
-$TargetPath = Resolve-Path "$BaseFolder\ExportFit\ExportFit\ExportFit.csproj"
-$ProjectXml.Save($TargetPath)
+#Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit\ExportFit.csproj /p:Configuration=Release /p:Platform=AnyCPU /t:publish /v:quiet /nologo"
 
-Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit\ExportFit.csproj /p:Configuration=Release /p:Platform=AnyCPU /t:publish /v:quiet /nologo"
+$username = Read-Host "Please enter your username"
+$password = Read-Host -assecurestring "Please enter your password"
 
-#Write-Host "Deploying updates to network server..."
-$LocalInstallerPath = (Resolve-Path "$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish").ToString() + "\*"
-$RemoteInstallerPath = "\\rddp-vm21\c$\temp"
-Copy-Item $LocalInstallerPath $RemoteInstallerPath -Recurse -Force
+#Upload to FTP site
 
-#Write-Host "Committing version increments to Perforce..."
-#p4 submit -d "Updating Executable ClickOnce Installer to version $newExeVersion" //my/project/tool/path/Executable/Executable.csproj | Out-Null
-#p4 submit -d "Updating Library to version $newLibVersion" //my/project/tool/path/Library/Properties/AssemblyInfo.cs | Out-Null
-#p4 submit -d "Updating Execu
+#rm -r directory
+c:\bin\curl.exe --user $username:$password ftp://$username.net/  -X 'DELE /public_html/apps/Application%20Files/ExportFit_*'
+
+$file = "$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\ExportFit.application"
+c:\bin\curl.exe --user $username:$password -v -T $file ftp://$username.net//public_html/apps/
+
+$files = @( "$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\ExportFit.exe.config.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\ExportFit.exe.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\ExportFit.exe.manifest",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\Fit.dll.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\FitUtils.dll.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\mp3device.ico.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\smartmed.ico.deploy" )
+
+foreach($file in $files)
+{
+	Write-Host "Uploading file " $file
+	c:\bin\curl.exe --user $username:$password -v -T $file ftp://$username.net//public_html/apps/Application%20Files/ExportFit_1_0_0_0/
+}
