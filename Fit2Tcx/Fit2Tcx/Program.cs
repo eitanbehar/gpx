8a48ef41e622e245a01276f396f109f31eab528a
Eitan Behar <eitan.behar@allscripts.com>
2014-06-30 07:21:33 +0300

    fix overflow

--- Fit2Tcx/Fit2Tcx/Program.cs
+++ Fit2Tcx/Fit2Tcx/Program.cs
@@ -14,13 +14,13 @@ namespace Fit2Tcx
         static void Main()
         {
 
-            string fitFile = @"c:\temp\2013-12-28-07-58-47.fit";
+            string fitFile = @"c:\temp\2014-06-14-07-01-42.fit";
             string tcxFile = Path.Combine(Path.GetDirectoryName(fitFile), Path.GetFileNameWithoutExtension(fitFile) + ".tcx");
 
             Tcx tcx = Fit.ReadFitFileIntoTcxObject(fitFile);
 
            ////actual distance, read from device, to be calculated in the future
-            tcx.DistanceMeters = 31440;
+            tcx.DistanceMeters = 19200;
            // tcx.Calories = 389; // TODO - read this from FIT
             
             int numberOfTrackPoints = tcx.TrackpointList.Count();
@@ -32,13 +32,13 @@ namespace Fit2Tcx
             for (int i = 0; i < tcx.TrackpointList.Count(); i++)
             {
                 tcx.TrackpointList[i].LongitudeDegrees = Utils.FitUnitsToDecimalDegrees(tcx.TrackpointList[i].LongitudeDegrees);
-                if (tcx.TrackpointList[i].LongitudeDegrees > 90)
+                if (tcx.TrackpointList[i].LongitudeDegrees > 90 && i > 0)
                 {
                     tcx.TrackpointList[i].LongitudeDegrees = tcx.TrackpointList[i-1].LongitudeDegrees;
                 }
 
                 tcx.TrackpointList[i].LatitudeDegrees = Utils.FitUnitsToDecimalDegrees(tcx.TrackpointList[i].LatitudeDegrees);
-                if (tcx.TrackpointList[i].LatitudeDegrees > 90)
+                if (tcx.TrackpointList[i].LatitudeDegrees > 90 && i > 0)
                 {
                     tcx.TrackpointList[i].LatitudeDegrees = tcx.TrackpointList[i-1].LatitudeDegrees;
                 }
