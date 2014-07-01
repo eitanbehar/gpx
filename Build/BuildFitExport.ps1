d9e0411d69c5948990fb6fb41ce891c7afaa581d
Eitan Behar <eitanb@dbmotion.com>
2014-07-01 18:01:38 +0300

    Adding publishing to BinTray

--- Build/BuildFitExport.ps1
+++ Build/BuildFitExport.ps1
@@ -2,15 +2,15 @@
 
 Write-Host "Tool Build/Deployment Script"
 
-$BaseFolder = 'C:\Personal\gpx'
+$BaseFolder = 'c:\personal\gpx'
 
 $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.EXE"
 
-#Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit.sln /p:Configuration=Release /p:Platform='Any CPU' /t:clean /v:quiet /nologo"
-#Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit.sln /p:Configuration=Release /p:Platform='Any CPU' /t:rebuild /v:quiet /nologo"
+Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit.sln /p:Configuration=Release /p:Platform='Any CPU' /t:clean /v:quiet /nologo"
+Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit.sln /p:Configuration=Release /p:Platform='Any CPU' /t:rebuild /v:quiet /nologo"
 
-$newExeVersion = '1.0.0.0'
-$newExeVersionFolder = '1_0_0_0'
+$newExeVersion = '1.0.11.0'
+$newExeVersionFolder = '1_0_11_0'
 
 #$ProjectXml = [xml](Get-Content $BaseFolder\ExportFit\ExportFit\ExportFit.csproj)
 #$ns = new-object Xml.XmlNamespaceManager $ProjectXml.NameTable
@@ -20,29 +20,34 @@ $newExeVersionFolder = '1_0_0_0'
 #$TargetPath = Resolve-Path "$BaseFolder\ExportFit\ExportFit\ExportFit.csproj"
 #$ProjectXml.Save($TargetPath)
 
-#Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit\ExportFit.csproj /p:Configuration=Release /p:Platform=AnyCPU /t:publish /v:quiet /nologo"
+Invoke-Expression "$msbuild $BaseFolder\ExportFit\ExportFit\ExportFit.csproj /p:Configuration=Release /p:Platform=AnyCPU /t:publish /v:quiet /nologo"
 
-$username = Read-Host "Please enter your username"
-$password = Read-Host -assecurestring "Please enter your password"
+$credential = Get-Credential -UserName baconao -Message Username
 
-#Upload to FTP site
+Write-Host "Deleting Previous Version"
+Invoke-RestMethod -Method Delete -Uri https://api.bintray.com/packages/baconao/tools/ExportFIT/versions/1.0.0.0 -Credential $credential
 
-#rm -r directory
-c:\bin\curl.exe --user $username:$password ftp://$username.net/  -X 'DELE /public_html/apps/Application%20Files/ExportFit_*'
+$file = $BaseFolder + '\ExportFit\ExportFit\bin\Release\app.publish\ExportFit.application'
+$uri = 'https://api.bintray.com/content/baconao/tools/ExportFIT/1.0.0.0/ExportFit.application'
 
-$file = "$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\ExportFit.application"
-c:\bin\curl.exe --user $username:$password -v -T $file ftp://$username.net//public_html/apps/
+Write-Host "Uploading file " $file
+Invoke-RestMethod -Method Put -Uri $uri -InFile $file -Credential $credential 
 
-$files = @( "$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\ExportFit.exe.config.deploy",
-			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\ExportFit.exe.deploy",
-			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\ExportFit.exe.manifest",
-			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\Fit.dll.deploy",
-			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\FitUtils.dll.deploy",
-			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\mp3device.ico.deploy",
-			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_1_0_0_0\smartmed.ico.deploy" )
+$files = @( "$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_$newExeVersionFolder\ExportFit.exe.config.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_$newExeVersionFolder\ExportFit.exe.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_$newExeVersionFolder\ExportFit.exe.manifest",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_$newExeVersionFolder\Fit.dll.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_$newExeVersionFolder\FitUtils.dll.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_$newExeVersionFolder\mp3device.ico.deploy",
+			"$BaseFolder\ExportFit\ExportFit\bin\Release\app.publish\Application Files\ExportFit_$newExeVersionFolder\smartmed.ico.deploy" )
 
 foreach($file in $files)
 {
 	Write-Host "Uploading file " $file
-	c:\bin\curl.exe --user $username:$password -v -T $file ftp://$username.net//public_html/apps/Application%20Files/ExportFit_1_0_0_0/
+	$name = [System.IO.Path]::GetFileName($file)
+	$uri = 'https://api.bintray.com/content/baconao/tools/ExportFIT/1.0.0.0/Application Files/ExportFit_' + $newExeVersionFolder + '/' + $name	
+	Invoke-RestMethod -Method Put -Uri $uri -InFile $file -Credential $credential 	
 }
+
+Write-Host "Publishing files"
+Invoke-RestMethod -Method Post -Uri https://api.bintray.com/content/baconao/tools/ExportFIT/1.0.0.0/publish -Credential $credential 
