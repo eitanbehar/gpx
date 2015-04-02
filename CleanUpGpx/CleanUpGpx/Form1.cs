13c0ea7d6437a9b49dca53cabfc3558d4a9a1dc4
Eitan Behar <eitan.behar@allscripts.com>
2015-04-02 22:22:05 +0300

    add gpx cleanup

---
+++ CleanUpGpx/CleanUpGpx/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CleanUpGpx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName = @"D:\Batey\Downloads\Pura 08-nov-2014.gpx";
+            string outFile = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName) + ".new.gpx");
+
+            XDocument xDoc = XDocument.Load(fileName);
+
+            XNamespace ns = xDoc.Root.Name.Namespace;
+
+            xDoc.Descendants(ns + "extensions").Remove();
+
+            xDoc.Save(outFile);
+
+        }
+    }
+}
